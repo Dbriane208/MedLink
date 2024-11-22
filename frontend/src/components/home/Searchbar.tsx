3514e878dcb754a64b189bc7576fa9681fb253f1
Daniel Brian <db9755949@gmail.com>
2024-11-22 23:36:24 +0300

    ADD: Implemented the admin and doctors dashboards

--- frontend/src/components/home/Searchbar.tsx
+++ frontend/src/components/home/Searchbar.tsx
@@ -1,14 +1,11 @@
-import {useState} from "react";
 import {IoIosSearch} from "react-icons/io";
 
 const ResponsiveSearchbar = () => {
 
-    const [inputFocus, setInputFocus] = useState(true)
-
     return (
         <div className="relative w-full sm:w-[80%] product_search_input">
             <input className="px-4 py-2 border border-border rounded-md w-full pl-[40px] outline-none"
-                   placeholder="Search..." onClick={() => setInputFocus(true)}/>
+                   placeholder="Search..." />
             <IoIosSearch className="absolute top-[9px] left-2 text-[1.5rem] text-[#adadad]"/>
         </div>
     );
